0c0b2387a7696205955fba60a6cf428160295867
unknown <dawidw22@gmail.com>
2022-05-10 11:01:33 +0200

    add rest of function

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const addUser = document.getElementById("add-user");
 const main = document.getElementById("main");
 const double = document.getElementById("double");
+const sort = document.getElementById("sort");
+const millioners = document.getElementById("show-millionaires");
+const wealth = document.getElementById("calculate-wealth");
 let data = [];
 
 addUser.addEventListener("click", getRandomUser);
 double.addEventListener("click", doubleMoney);
+sort.addEventListener("click", sortMoney);
+millioners.addEventListener("click", showMillioners);
+wealth.addEventListener("click", totalWealth);
 
 async function getRandomUser() {
   const res = await fetch("https://randomuser.me/api/");
@@ -31,7 +37,7 @@ function updateUi(provideData = data) {
   provideData.forEach((e) => {
     const element = document.createElement("div");
     element.classList.add("person");
-    element.innerHTML = `<strong>${e.name}</strong> ${e.money}`;
+    element.innerHTML = `<strong>${e.name}</strong> ${formatMoney(e.money)}`;
     main.appendChild(element);
   });
 }
@@ -45,4 +51,37 @@ function doubleMoney() {
   }));
   updateUi(newMoney);
   data = newMoney;
+}
+
+//Sort Money
+
+function sortMoney() {
+  const newMoney = data.sort((a, b) => {
+    return b.money - a.money;
+  });
+  updateUi(newMoney);
+  data = newMoney;
+}
+
+// Show only millioners
+
+function showMillioners() {
+  const newMoney = data.filter((element) => element.money >= 1000000);
+  updateUi(newMoney);
+  data = newMoney;
+}
+
+// total wealth
+
+function totalWealth() {
+  const total = data.reduce((acc, person) => (acc += person.money), 0);
+  const element = document.createElement("div");
+  element.innerHTML = `<h3>Total wealth: <strong>
+  ${formatMoney(total)}
+  </strong></h3>`;
+  main.appendChild(element);
+}
+
+function formatMoney(number) {
+  return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
 }
